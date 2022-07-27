cfdc325e28c2f851a7036312b6c4efc10606c46d
LandenStephenss <turtlederp806@icloud.com>
2022-07-27 14:00:26 -0500

    start clean

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,14 +1,5 @@
-/*
- * Copyright (c) 2018-2022 Replugged Developers
- * Licensed under the Open Software License version 3.0
- * SPDX-License-Identifier: OSL-3.0
- */
-
-import type { User } from './components/UserContext'
 import { h, render, hydrate } from 'preact'
-import { useState, useEffect } from 'preact/hooks'
 import App from './components/App'
-import { Endpoints } from './constants'
 import './main.css'
 
 declare global {
@@ -18,26 +9,27 @@ declare global {
 }
 
 function Wrapper () {
-  const [ user, setUser ] = useState<User | null | undefined>(void 0)
-  useEffect(() => {
-    if (window.HAS_TOKEN !== false) {
-      fetch(Endpoints.USER_SELF)
-        .then((r) => r.json())
-        .then((u) => {
-          if (u._id) {
-            const patch = (newUser: Partial<User>): void => setUser((oldUser) => <User> ({ ...oldUser, ...newUser, patch: patch }))
-            setUser({ ...u, patch: patch })
-            return
-          }
+  // this needs to get setup again, one day.
+  // const [ user, setUser ] = useState<User | null | undefined>(void 0)
+  // useEffect(() => {
+  //   if (window.HAS_TOKEN !== false) {
+  //     fetch(Endpoints.USER_SELF)
+  //       .then((r) => r.json())
+  //       .then((u) => {
+  //         if (u._id) {
+  //           const patch = (newUser: Partial<User>): void => setUser((oldUser) => <User> ({ ...oldUser, ...newUser, patch: patch }))
+  //           setUser({ ...u, patch: patch })
+  //           return
+  //         }
 
-          setUser(null)
-        })
-    } else {
-      setUser(null)
-    }
-  }, [])
+  //         setUser(null)
+  //       })
+  //   } else {
+  //     setUser(null)
+  //   }
+  // }, [])
 
-  return h(App, { user: user })
+  return h(App, null)
 }
 
 if (import.meta.env.DEV) {
