67f1ae7e1afce02564fafbdc02f740c6417b7254
Albert Portnoy <albert@albertp.dev>
2023-02-19 20:36:55 -0600

    Design is my passion

--- web/src/components/Download.tsx
+++ web/src/components/Download.tsx
@@ -3,6 +3,7 @@ import { h, JSX } from 'preact';
 import { useTitleTemplate } from 'hoofd/preact';
 
 import style from './download.module.css';
+import sharedStyle from './shared.module.css';
 import { useState } from 'preact/hooks';
 
 const DOWNLOAD_URL_BASE =  'https://github.com/replugged-org/electron-installer/releases/latest/download/';
@@ -50,7 +51,7 @@ const operatingSystems: OperatingSystemData[] = [ {
     Not all Linux distributions are supported. If the installer is not able to find your installation or you are using Flatpak, please follow the <a href="#manual">manual installation instructions</a>.
   </>,
   files: [ {
-    label: 'Download for x86_64',
+    label: 'Download for x86',
     file: 'replugged-installer-linux.AppImage'
   }, {
     label: 'Download for arm64',
@@ -67,41 +68,42 @@ export default function Homepage () {
 
   const selectedOSData = operatingSystems.find(os => os.os === selectedOS)!;
 
-  // TODO: styles for tabs, buttons, warning
   // TODO: manual installation instructions
 
   return (
     <main className={style.container}>
       <div className={style.heading}>
         <div className={style.wrapper}>
-          <h1 className={style.title}>Powerful and simple Discord client mod</h1>
-          <p className={style.motto}>Enhance your Discord experience with new features and looks. Make your Discord truly yours.</p>
-          <div className={style.tabs}>
-            {operatingSystems.map(os => (
-              <button
-                className={`${style.tab} ${os.os === selectedOS ? style.selected : ''}`}
-                onClick={() => setSelectedOS(os.os)}
-              >
-                {os.name}
-              </button>
-            ))}
-          </div>
-          <div className={style.buttons}>
-            {selectedOSData.files.map(file => (
-              <a
-                className={style.button}
-                href={`${DOWNLOAD_URL_BASE}/${file.file}`}
-                download
-              >
-                {file.label}
-              </a>
-            ))}
-          </div>
-          {selectedOSData.warning && (
-            <div className={style.warning}>
-              {selectedOSData.warning}
+          <h1 className={style.title}>Download Replugged</h1>
+          <div className={style.downloadContainer}>
+            <div className={style.tabs}>
+              {operatingSystems.map(os => (
+                <button
+                  className={`${style.tab} ${sharedStyle.button} ${os.os === selectedOS ? style.selected : ''}`}
+                  onClick={() => setSelectedOS(os.os)}
+                >
+                  {os.name}
+                </button>
+              ))}
             </div>
-          )}
+            <div className={style.divider} />
+            <div className={style.buttons}>
+              {selectedOSData.files.map(file => (
+                <a
+                  className={sharedStyle.button}
+                  href={`${DOWNLOAD_URL_BASE}/${file.file}`}
+                  download
+                >
+                  {file.label}
+                </a>
+              ))}
+            </div>
+            {selectedOSData.warning && (
+              <div className={style.warning}>
+                {selectedOSData.warning}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </main>
