5651e09c531fbd3d6ec556611fd77cab2a5bcf42
Albert Portnoy <albert@albertp.dev>
2023-02-20 21:09:03 -0600

    Send other requests to index.html

--- web/server.js
+++ web/server.js
@@ -9,3 +9,6 @@ app.listen(Number(process.env.PORT ?? 8000), () => {
 app.use(cors());
 
 app.use(express.static('dist'));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist' });
+});
